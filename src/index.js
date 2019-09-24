84f3eb31ee2856db8a8363b7515cdcd94a7dec9b
Farafonov <eu.farafonov@gmail.com>
2019-09-24 22:46:00 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  let two = 0;
+  let five = 0;
+  let arr = expression.split("*");
+
+  arr.forEach(function(el) {
+    let numb = parseInt(el);
+    let str = el.slice(el.indexOf("!"));
+
+    if (str == "!") {
+      five += findCount(numb, 5);
+      two += findCount(numb, 2);
+    }
+
+    if (str == "!!") {
+      if (numb % 2 == 0) {
+        two += findCount(numb, 2);
+      }
+
+      for (let i = numb; i > 1; i -= 2) {
+        let temp_var = i;
+        while (temp_var % 5 == 0) {
+          five++;
+          temp_var /= 5;
+        }
+      }
+    }
+  });
+
+  function findCount(num, div) {
+    let cnt = 0;
+    while (num > 0) {
+      cnt += Math.floor(num / div);
+      num /= div;
+    }
+    return cnt;
+  }
+
+  return five < two ? five : two;
+};
